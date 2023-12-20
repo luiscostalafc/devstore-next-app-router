e630a89446243d4eddd7f4a40f7fde3e18f9c7ae
Luis Costa <luiscostalafc@gmail.com>
2023-12-20 06:38:34 -0300

    feat: create Product page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -5,7 +5,11 @@ import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
 
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/products/featured");
+  const response = await api("/products/featured", {
+    next: {
+      revalidate: 60 * 60, // 1 hour
+    },
+  });
 
   const products = await response.json();
 
@@ -18,7 +22,7 @@ export default async function Home() {
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/products/${highlightedProduct.slug}`}
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 over-flow-hidden flex justify-center items-end"
       >
         <Image
@@ -46,7 +50,7 @@ export default async function Home() {
       {otherProducts.map((product) => (
         <Link
           key={product.id}
-          href={`/products/${product.slug}`}
+          href={`/product/${product.slug}`}
           className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 over-flow-hidden flex justify-center items-end"
         >
           <Image
